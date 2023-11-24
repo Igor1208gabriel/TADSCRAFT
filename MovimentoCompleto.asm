72a0d8637b303e0cf6940119f77ee9b5f61802bd
= <santos.igor1@academico.ifrn.edu.br>
2023-11-24 07:22:58 -0300

    DEU CERTO!!!

--- MovimentoCompleto.asm
+++ MovimentoCompleto.asm
@@ -19,16 +19,15 @@ telainicial:
 	j mortadela
 	
 lado:
-	lui $9 0x1001
-	addi $9 $9 32768
+	addi $9 $9 -98304
 	addi $10 $0 8192
 	addi $24 $0 1
 	lui $8 0x1001
 	j mortadela
 	
 frente:
-	lui $9 0x1001
-	addi $9 $9 131072
+	
+	addi $9 $9 98304
 	addi $10 $0 8192
 	addi $24 $0 0
 	lui $8 0x1001
@@ -41,6 +40,7 @@ mortadela: #função básica para mostrar a tela
 	addi $8  $8 4
 	addi $9  $9 4
 	bne  $10 $0 mortadela
+	addi $9 $9 -32768
 	j esperar
 
 esperar: #while(true) // if(tecla_apertada) break
@@ -51,15 +51,19 @@ esperar: #while(true) // if(tecla_apertada) break
 mudou: #um tanto de if pra ver qual tecla foi apertada
 	lw   $25 4($14)
 	addi $13 $0 32 #valor do espaço
-	beq  $25 $13 frente
+	beq  $25 $13 inicio
+	
 	addi $13 $0 'd'
-	beq $25 $13 ladooufrente
+	beq  $25 $13 ladooufrente
+	
 	addi $13 $0 'a'
-	beq $25 $13 ladooufrente
+	beq  $25 $13 ladooufrente
+	
 	addi $13 $0 'w'
-	beq $25 $23 cima
+	beq  $25 $13 cima
+	
 	addi $13 $0 's'
-	beq $25 $13 baixo
+	beq  $25 $13 baixo
 	j esperar
 
 ladooufrente:
@@ -70,12 +74,18 @@ ladooufrente:
 cima:
 	addi $10 $0 8192
 	lui $8 0x1001
-	addi $9 $9 -512
+	addi $9 $9 -1024
 	j mortadela
 
 baixo:
 	addi $10 $0 8192
 	lui $8 0x1001
-	addi $9 $9 512
+	addi $9 $9 1024
+	j mortadela
+	
+inicio:
+	addi $10 $0 8192
+	lui $8 0x1001
+	addi $9 $9 161792
 	j mortadela
 	

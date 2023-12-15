eab8a3ffd341408138c30774071640bfd9846dfd
Igor gabriel dos santos <20231014040009@ifrn.local>
2023-12-15 08:32:40 -0300

    Testes para pverlay da mão

--- Novo/Main.asm
+++ Novo/Main.asm
@@ -10,59 +10,69 @@
 .include "08.asm"
 .include "09.asm"
 .include "10.asm"
+.include "mao.asm"
 
 .text
 paocima:
-	lui  $8 		0x1001	#Ponteiro "para"
-	lui  $9 		0x1001	#Ponteiro "de"
-	lui  $10 		0xffff		#Visualizador de inputs
+	lui    $8 		0x1001	#Ponteiro "para"
+	lui    $9 		0x1001	#Ponteiro "de"
+	lui    $10 		0xffff		#Visualizador de inputs 
+	lui    $16 		0x1001
+	addi $16 $0 	18874368
+	addi $17 $0 50
 	addi $11 $0 	0		#"Carregador" de $9 para $8
 	addi $12 $0 	32768	#Contador do tamanho de uma tela (256*384)
 	addi $13 $0 	0		#"Qual tela" 
 	addi $14 $0 	10		#"Quantas telas"
 	addi $15 $0 	393216 	#$12 * 4
-	j mortadela
+	j teste #mortadela
 	
-picles:				#mostra o meio de "um" quando clicar no espaco
-	addi $13 $0 	1
-	lui  $9 	0x1001	#reseta 9 pra posicao inicial 
-	addi $9 $9 	262144	#ajusta 9 na posicao correta
+teste:
+	addi $17 $17 -1
+	lw $11 0($16)
+	sw $11 0($8)
+	
+	
+picles:					#mostra o meio de "um" quando clicar no espaco
+	addi $13 $0 		1
+	lui  $9 		0x1001	#reseta 9 pra posicao inicial 
+	addi $9 $9 		262144	#ajusta 9 na posicao correta
 	j mortadela
 	
 		
-alface:				#funcao "wait"
-	lw  $11    0($10)		#checa se houve input
+alface:					#funcao "wait"
+	lw  $11    0($10)			#checa se houve input
 	bne $11 $0 queijo		
 	j alface
 
-ketchup:				#sobe a tela
+ketchup:					#sobe a tela
 	addi $9 $9 -8192
 	j mortadela
 
-mostarda:				#desce a tela
+mostarda:					#desce a tela
 	addi $9 $9  8192
 	j mortadela
 	
-queijo:				#checa qual foi o input e vai para a fun��o correspondente
+queijo:					#checa qual foi o input e vai para a fun��o correspondente
 	lw  $11     4($10)
 	beq $11 'w' rucula
 	beq $11 'd' hamburguer
 	beq $11 's' tomate
-	beq $11 'a' tomate
+	beq $11 'a' ovofrito
 	beq $11 ' ' picles
 	j alface
 	
 mortadela:
 	addi $12 $12 -1		
-	lw   $11      0($9)	#bota "de" no 11
-	sw   $11      0($8)	#bota de 11 no "para"
-	addi $8  $8   4		#proximo indice do vetor
+	lw   $11      0($9)		#bota "de" no 11
+	sw   $11      0($8)		#bota de 11 no "para"
+	addi $8  $8   4			#proximo indice do vetor
 	addi $9  $9   4		
-	bne  $12 $0   mortadela #condicao do loop
+	bne  $12 $0   mortadela	#condicao do loop
 	
-	addi $9 $9   -131072	#$8 e $9 para seus valores antes de mortadela
+	addi $9 $9   -131072		#$8 e $9 para seus valores antes de mortadela
 	addi $8 $8   -131072	
-	addi $12 $0   32768	#volta $12 para seu valor antes de mortadela
+	addi $12 $0   32768		#volta $12 para seu valor antes de mortadela
 	j alface
 
 hamburguer:			
